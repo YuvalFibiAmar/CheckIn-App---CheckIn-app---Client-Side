e5c2bf64f4b7f900f58586c423f51165311a7f88
yuval amar <117682807+YuvalFibiAmar@users.noreply.github.com>
2022-11-27 14:09:41 +0200

    Add files via upload

--- jsPage.js
+++ jsPage.js
@@ -1,20 +1,16 @@
 'use strict';
 
 // 1. ----------------------------------------------------------initialization -----> Objects , btns-------------------------------------------------------------
-let flag = false;
 
-//1.a modal btns :
 const modal1 = document.querySelector('.modal1');
 const overlay = document.querySelector('overlay');
 const btnopen1 = document.querySelector('.show-modal1');
 const btnclos1 = document.querySelector('.close-modal1');
 
-// 1.b  ----- > checkIn btns
 const btnCheckIn = document.querySelector('.btn');
 const fullName = document.querySelector('.login__input--user');
 const fullPassport = document.querySelector('.login__input--passport');
 
-// 1. 3 ---- > Passenger object
 const Passenger = {
   firstName: 'null',
   passportNum: 0,
@@ -35,14 +31,12 @@ const Passenger = {
   },
 };
 
-// 1.4 -----> List of Passengers who did checkIn
-
 const PassengerList = {
   names: ['Daniel', 'David', 'Yael', 'Naomi', 'Shai'],
   passNumbers: ['318620721', '8315609', '123456789', '052808097', '0524613558'],
 };
 
-//2 . 1 ----------------------------------------------------------------------- >  main --------------------------------------------------------------- :
+//2 ----------------------------------------------------------------------- >  main --------------------------------------------------------------- :
 if (
   btnopen1.addEventListener('click', function () {
     console.log('button clicked');
@@ -83,12 +77,10 @@ if (
 function checkIsExist(passportnumber) {
   for (let i = 0; i < PassengerList.passNumbers.length; i++) {
     if (passportnumber === PassengerList.passNumbers[i]) {
-      flag = true;
-      break;
+      return true;
     }
   }
-
-  return flag;
+  return false;
 }
 
 function updatePassengersList(nameValue, passportValue) {
